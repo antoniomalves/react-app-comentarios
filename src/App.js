42e5a0544b25dc5b4376beb7ea5d1031d718490c
Antonio M Alves Lima <antonio.malves.lima@gmail.com>
2018-01-27 18:15:56 -0200

    Removido arquivos desnecessários e importacao do bootstrap no projeto.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
+import 'bootstrap-css-only'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Sistema de comentários</h1>
+          <p><button className="btn btn-info">Teste</button></p>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
