59edce4a9095af2a9b2990002e70f4714bedf17e
Antonio M Alves Lima <antonio.malves.lima@gmail.com>
2018-01-28 19:43:35 -0200

    inclusao das dependencias para testes e realizacao do primeiro teste.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import {shallow, mount, render} from 'enzyme'
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.length).toBe(1)
+  })
+  it('should have .container class', () =>{
+    const wrapper = shallow(<App />)
+    expect(wrapper.is('.container')).toBe(true)
+  })
+  it('shows comments', () =>{
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('Comments').length).toBe(1)
+  })
+  it('shows NewComment', () =>{
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('NewComment').length).toBe(1)
+  })
+/*   it('outputs the <App />', () =>{
+    const wrapperShallow  = shallow(<App />)
+    const wrapperMount    = mount(<App />)
+    const wrapperRender   = render(<App />)
+
+    console.log(wrapperShallow.debug())
+    console.log(wrapperMount.debug())
+    console.log(wrapperRender.html())
+  }) */
+
+
+})
