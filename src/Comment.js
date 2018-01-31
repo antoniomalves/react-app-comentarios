82fcbe09c36aaa7894b2f88394119a4687cb5168
Antonio M Alves Lima <antonio.malves.lima@gmail.com>
2018-01-30 23:52:09 -0200

    Associando comentário a usuario do facebook logado.

--- src/Comment.js
+++ src/Comment.js
@@ -3,7 +3,10 @@ import React from 'react'
 
 //functional stateless components
 
-const Comment = props => <p className="well well-sm">{props.comment.comment}</p>
-  
+const Comment = props => 
+  <p className="well well-sm">
+    {props.comment.comment} <br/>
+   <b>por: {props.comment.user.name}</b>
+   </p>
 
 export default Comment
